7585d5fadeaa935c5cd736f75ee67bcbbe2ff816
yuwo <424576805@qq.com>
2023-03-13 22:16:16 +0800

    倒计时组件

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -1,12 +1,22 @@
 Page({
-  data: {
-    countDownVisible: true,
-  },
+	data: {
+		countDownVisible: false // 控制是否显示倒计时
+	},
 
-  countDownChange(ev) {
-    this.setData({
-      timeData: ev.detail,
-      countDownVisible: ev.detail.minutes === 1 || ev.detail.seconds > 0,
-    })
-  },
+	// 倒计时变化执行的函数
+	countDownChange(ev) {
+		console.log(ev)
+		this.setData({
+			timeData: ev.detail, // 倒计时的数据（倒计时的时间：时分秒等）
+			// 当倒计时分钟数等于1（起始时间1分钟） 或者 秒数大于0 ，显示倒计时，不满足条件则为关闭倒计时
+			countDownVisible: ev.detail.minutes === 1 || ev.detail.seconds > 0
+		})
+	},
+
+	// 点击 <获取验证码> 按钮执行的函数
+	getCode() {
+		this.setData({
+			countDownVisible: true // 显示倒计时
+		})
+	}
 })
