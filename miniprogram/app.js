ef0a43ae2910859d7561bfb5f4ae1840b3af89c7
yuwo <424576805@qq.com>
2023-03-13 20:55:50 +0800

    App从本地获取token

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,5 +2,28 @@
 import './utils/utils'
 import './utils/http'
 App({
-  globalData: {},
+	data: {
+		token: ''
+	},
+	// 小程序启动时执行,仅执行一次
+	onLaunch() {
+		console.log('小程序启动')
+		// 读取token
+		this.getToken('token')
+	},
+	// 获取token的方法
+	getToken(key) {
+		wx.getStorage({
+			key,
+			// 获取成功执行的函数
+			success: ({ data }) => {
+				console.log(data)
+				this.token = data
+			}
+			// 获取失败执行的函数
+			// fail: (err) => {
+			// 	console.log(err, '~~~获取token失败')
+			// }
+		})
+	}
 })
